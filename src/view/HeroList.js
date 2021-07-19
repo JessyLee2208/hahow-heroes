e167f938c28ab020c35231995b2d3dff417477e7
jessylee2208 <jessylee2208@gmail.com>
2021-07-19 10:05:06 +0800

    comment out

--- src/view/HeroList.js
+++ src/view/HeroList.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import HeroeCards from '../components/HeroeCards';
 import useHeroDataCheck from '../hook/useHeroDataCheck';
-import { Loading } from '../components/LottieAnimat';
 
 const CardList = styled.div`
   display: flex;
@@ -17,21 +16,12 @@ const CardList = styled.div`
 
 function HeroList() {
   const [selected, setSelected] = useState(false);
-  // const [loaderShow, setLoaderShow] = useState(true);
   const host_name = `https://hahow-recruit.herokuapp.com/heroes`;
 
   const heroes = useHeroDataCheck(host_name);
   function handler(e) {
     setSelected(e.target.id);
   }
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setLoaderShow(false);
-  //   }, 3000);
-
-  //   return () => clearTimeout(timer);
-  // }, []);
 
   return (
     <>
